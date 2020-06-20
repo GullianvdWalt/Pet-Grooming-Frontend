e2a35f27d4c7e1c4cf148166f418bf1a8957786c
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-06-20 09:12:42 +0200

    Customer Edit Modal Form Control Added

--- static/js/customer.js
+++ static/js/customer.js
@@ -26,3 +26,31 @@ window.onclick = function(event) {
   }
 }
 
+/* Add New Pet Alert*/
+function addPetFunction(){
+  alert("Enter Pet Details In Highlighted Fields.\n"
+         + "Click The Save Button When Done.");
+ document.getElementsByClassName("pet-input");
+  /* Hilighted Fields */
+  document.getElementById("pet-name-input").style.backgroundColor = "hotpink";
+  document.getElementById("pet-input-breed").style.backgroundColor = "hotpink";
+  document.getElementById("gender-male-label").style.color = "hotpink";
+  document.getElementById("gender-female-label").style.color = "hotpink";
+}
+
+/*Reset Highlited Fields*/ 
+function resetNameFunction(){
+  document.getElementById("pet-name-input").style.backgroundColor = "white";
+}
+
+function resetBreedFunction(){
+  document.getElementById("pet-input-breed").style.backgroundColor = "white";
+}
+
+function resetGenderFunction(){
+  document.getElementById("gender-male-label").style.color = "#505050";
+  document.getElementById("gender-female-label").style.color = "#505050";
+}
+    
+
+
